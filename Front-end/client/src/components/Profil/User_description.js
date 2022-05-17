83665e5f8aa8624e0fd16a587c1490a95f5348cd
Grégoire Paulet <gregoirepaulet84@gmail.com>
2022-05-17 18:48:25 +0200

    Réalisation du component info user

--- Front-end/client/src/components/Profil/User_description.js
+++ Front-end/client/src/components/Profil/User_description.js
@@ -1,30 +1,52 @@
 import React from 'react';
 import profilPic from '../../images/Photos de profil groupomania/zeus_arte.PNG';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot} from '@fortawesome/free-solid-svg-icons'
 
 
 const User_description = () => {
     return (
         <section className='user-description-box'>
-            <div className="profil-pic">
-                <img src={profilPic} alt="Photo de profil de l'utilisateur" />
+            <div className="profil-pic-name_box">
+                <div className="profil-pic">
+                    <img src={profilPic} alt="Photo de profil de l'utilisateur" />
+                </div>
+                <div className="user-name">
+                    <h2>Zeus, Dieu des dieux</h2>
+                    <FontAwesomeIcon icon={ faLocationDot } /><span>Palais de l'olympe</span>
+                </div>
             </div>
             <div className="biography">
-                <h3 className="biography-title">
-                    Biographie
-                </h3>
+                <div className="title-line">
+                    <h3 className="biography-title">
+                        Biographie
+                    </h3>
+                    <div className="purple-line"></div>
+                </div>
                 <div className="biography-content">
                     "Après 30 ans dans le secteur de l'électricité, me voici fraîchement débarqué chez Groupomania"
                 </div>
             </div>
             <div className="informations">
-                <h3 className="informations-title">
-                    Informations
-                </h3>
+                <div className="title-line">
+                    <h3 className="informations-title">
+                        Informations
+                    </h3>
+                    <div className="purple-line"></div>
+                </div>
                 <div className="informations-content">
-                    <p>Date de naissance:</p><span>15 janvier 1956</span>
-                    <p>Email:</p><span>zeus@gmail.com</span>
-                    <p>Téléphone:</p><span>+33673519463</span>
-                    <p>Adresse:</p><span>3 rue Pégase, Olympe</span>
+                    <div className="info-box">
+                        <span className='info-key'>Date de naissance:</span><span className='info-value'>15 janvier 1956</span>
+                    </div>
+                    <div className="info-box">
+                        <span className='info-key'>Email:</span><span className='info-value'>zeus@gmail.com</span>
+                    </div>
+                    <div className="info-box">
+                        <span className='info-key'>Téléphone:</span><span className='info-value'>+33673519463</span>
+                    </div>
+                    <div className="info-box">
+                        <span className='info-key'>Adresse:</span><span className='info-value'>3 rue Pégase, Olympe</span>
+                    </div>
                 </div>
             </div>
         </section>
