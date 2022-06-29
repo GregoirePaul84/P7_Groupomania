531ea32e3ba2d1e947e0a98b16da51e9a167be65
Grégoire Paulet <gregoirepaulet84@gmail.com>
2022-06-29 14:35:44 +0200

    ajout du composant CardHome afin d'éviter l'itération de useState lors du map

---
+++ Front-end/src/components/Home/CardHome.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane, faThumbsUp, faMessage, faThumbsDown, faTrashCan, faPen} from '@fortawesome/free-solid-svg-icons';
+import { convertTime } from '../../App';
+
+const CardHome = ({postsObject, allUsersResults, elt}) => {
+
+    // const [greenActive, setGreenActive] = useState(true);
+    // const [redActive, setRedActive] = useState(true);
+    // const [visibility, setVisibility] = useState(true);
+    const [isUpdated, setIsUpdated] = useState(false);
+    const [textUpdate, setTextUpdate] = useState(null);
+    // const [showPicture, setShowPicture] = useState(false);
+    console.log(postsObject);
+    console.log(allUsersResults);
+    console.log(elt);
+
+        let imgUrl = null;
+
+        for (let i in postsObject) {
+
+            imgUrl = postsObject[i].image_url;
+
+            // On récupère les infos utilisateurs dont l'userID est présente dans les posts
+            const filterUsersPosts = (allUsersResults.filter((elt) => elt.user_id === postsObject[i].user_id));
+            const postId = postsObject[i].post_id;
+            const selectCards = document.querySelector(`.post_id${postId}`);
+            if (selectCards !== null) {
+                const selectImg = selectCards.querySelector('.profil-pic');
+                selectImg.setAttribute('src', `${filterUsersPosts[0].profil_pic}`);
+
+                const selectH3 = selectCards.querySelector('.user-name');
+                selectH3.textContent = `${filterUsersPosts[0].first_name}, ${filterUsersPosts[0].last_name}`;
+
+                const selectEmail = selectCards.querySelector('.email');
+                selectEmail.textContent = `${filterUsersPosts[0].email}`;
+            }
+        }
+    
+
+    const updateItem = () => {
+        // if(textUpdate) {
+        //     dispatch(updatePost(postId, textUpdate, userId))
+        //         .then(() => setIsUpdated(false))
+        //         .then(() => dispatch(getUserPosts(userId)));
+        // }
+    }
+
+    return (
+        <div className={`card-container post_id${elt.post_id}`} key={elt.post_id}> 
+            <div className="card-smallContainer">
+                <div className="card-user-picture">
+                    <img alt="utilisateur" src={""} className="profil-pic"/>
+                </div>
+                <div className="card-name-user">
+                    <h3 className='user-name'></h3>
+                    <p className='email'></p>
+                </div>
+                <div className="modify-delete">
+                    <FontAwesomeIcon icon={faPen} onClick={() => setIsUpdated(!isUpdated)}/>
+                    <FontAwesomeIcon icon={faTrashCan} />
+                </div>
+                <div className="card-message">
+                    <FontAwesomeIcon icon={ faPaperPlane } />
+                    <span className="post-date">{convertTime(elt.created)}</span>
+                    <div className="post-content">
+                        {isUpdated === false && <div className='message'>{elt.text}</div>}
+                        {isUpdated && (
+                        <div className="update-post">
+                            <textarea
+                                className="update-content"
+                                defaultValue={""}
+                                onChange={ (e) => setTextUpdate(e.target.value)}
+                            />
+                            <div className="update-button">
+                                <button className='btn' onClick={updateItem}>Modifier</button>
+                            </div>
+                        </div>
+                        )}
+                    </div>
+                    {/* { (imgUrl) ? 
+                    <div className='picture'>
+                        <img src={imgUrl} alt="post utilisateur" />
+                    </div>
+                    : <div></div>} */}
+                </div>
+                <div className="card-likes-posts">
+                    <FontAwesomeIcon icon={ faMessage } />
+                    <FontAwesomeIcon icon={ faThumbsUp } />
+                    <FontAwesomeIcon icon={ faThumbsDown } />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CardHome;
--- Front-end/src/components/Home/NewsFeed.js
+++ Front-end/src/components/Home/NewsFeed.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllPosts } from '../../actions/post.actions';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPaperPlane, faThumbsUp, faMessage, faThumbsDown, faTrashCan, faPen} from '@fortawesome/free-solid-svg-icons';
-import { convertTime } from '../../App';
+import { faMessage } from '@fortawesome/free-solid-svg-icons';
 import { getAllUsers } from '../../actions/user.actions';
+import CardHome from './CardHome';
 
 const NewsFeed = () => {
 
@@ -19,36 +19,10 @@ const NewsFeed = () => {
     const allUsersData = useSelector((state) => state.userAllReducer);
     const allUsersResults = allUsersData.results;
 
-    // const [greenActive, setGreenActive] = useState(true);
-    // const [redActive, setRedActive] = useState(true);
-    // const [visibility, setVisibility] = useState(true);
-    const [isUpdated, setIsUpdated] = useState(false);
-    // const [textUpdate, setTextUpdate] = useState(null);
-    // const [showPicture, setShowPicture] = useState(false);
-
     let postsObject = [];
 
     if (Object.keys(allPostsData).length !== 0) {
         postsObject = allPostsData.results;
-        console.log(postsObject);
-
-        for (let i in postsObject) {
-
-            // On récupère les infos utilisateurs dont l'userID est présente dans les posts
-            const filterUsersPosts = (allUsersResults.filter((elt) => elt.user_id === postsObject[i].user_id));
-            const postId = postsObject[i].post_id;
-            const selectCards = document.querySelector(`.post_id${postId}`);
-            if (selectCards !== null) {
-                const selectImg = selectCards.querySelector('.profil-pic');
-                selectImg.setAttribute('src', `${filterUsersPosts[0].profil_pic}`);
-
-                const selectH3 = selectCards.querySelector('.user-name');
-                selectH3.textContent = `${filterUsersPosts[0].first_name}, ${filterUsersPosts[0].last_name}`;
-
-                const selectEmail = selectCards.querySelector('.email');
-                selectEmail.textContent = `${filterUsersPosts[0].email}`;
-            }
-        }
     }
     else {
         return;
@@ -57,35 +31,9 @@ const NewsFeed = () => {
     return (
         <div className='news-feed'>
             { (postsObject.length !== 0) ?
-                postsObject.map((key) => {
+                postsObject.map((elt) => {
                     return (
-                        <div className={`card-container post_id${key.post_id}`} key={key.post_id}> 
-                            <div className="card-smallContainer">
-                                <div className="card-user-picture">
-                                    <img alt="utilisateur" src={""} className="profil-pic"/>
-                                </div>
-                                <div className="card-name-user">
-                                    <h3 className='user-name'></h3>
-                                    <p className='email'></p>
-                                </div>
-                                <div className="modify-delete">
-                                    <FontAwesomeIcon icon={faPen} />
-                                    <FontAwesomeIcon icon={faTrashCan} />
-                                </div>
-                                <div className="card-message">
-                                    <FontAwesomeIcon icon={ faPaperPlane } />
-                                    <span className="post-date">{convertTime(key.created)}</span>
-                                    <div className="post-content">
-                                        {isUpdated === false && <div className='message'>{key.text}</div>}
-                                    </div>
-                                </div>
-                                <div className="card-likes-posts">
-                                    <FontAwesomeIcon icon={ faMessage } />
-                                    <FontAwesomeIcon icon={ faThumbsUp } />
-                                    <FontAwesomeIcon icon={ faThumbsDown } />
-                                </div>
-                            </div>
-                        </div>
+                        <CardHome postsObject={postsObject} allUsersResults={allUsersResults} key={elt.post_id} elt={elt}/>
                     )
                 })
                 : <div className='no-post'>
